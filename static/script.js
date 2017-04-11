467ff05a8487e5f2bb784d5aebcb42e421428b3b
masterxyth <rizwanq@sfu.ca>
2017-04-10 21:22:21 -0700

    uploading code

---
+++ static/script.js
@@ -0,0 +1,35 @@
+function search() {
+    // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementsByClassName("dataframe data");
+    tr = document.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td1 = tr[i].getElementsByTagName("td")[0];
+        td2 = tr[i].getElementsByTagName("td")[1];
+        td3 = tr[i].getElementsByTagName("td")[2];
+        if (td1) {
+            if (td1.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else {
+              if (td2) {
+                  if (td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                      tr[i].style.display = "";
+                    } else {
+                      if (td3) {
+                        if (td3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+}
