c7557e0e6f3dc52c02663af70a0bd08b4f383328
masterxyth <rizwanq@sfu.ca>
2017-04-15 13:36:23 -0700

    updated with google analytics

---
+++ conversion.py
@@ -0,0 +1,26 @@
+import csv, json
+
+
+'''
+Output {
+
+Riding Id: x,
+Riding name: x,
+Riding candidates:
+{ name 1: x,
+          { party:x,}
+Name 2:x,
+
+{
+    party:x,}
+
+Name 3:x,
+
+
+{     party:x,}
+Name 4:
+
+{     party: x, }
+}
+}
+'''
