e773f1963b2cf9323be07973d3d2da20ac3c5973
masterxyth <rizwanq@sfu.ca>
2017-04-15 13:46:30 -0700

    changed /tables to / in Flask to set homepage direct to tables

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/tables") #Redirect link
+@app.route("/") #Redirect link
 
 def show_tables():
     data = pd.read_csv('standing-nominees.CSV') #reading the CSV file
     data.set_index(['candidate_id'], inplace=True) # Setting the first column as the index
     data.index.name=None #Removing column header name
-    return render_template('view.html',tables=[data.to_html(classes='data')], #returning view.html file from templates, using the data returned as a table in html with Class Data
+    return render_template('index.html',tables=[data.to_html(classes='data')], #returning view.html file from templates, using the data returned as a table in html with Class Data
     titles = ['Candidates']) #Table Title
 
 if __name__ == "__main__":
