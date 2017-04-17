b7f1663c0a90bd936fc2c7fc1e5a2d048d81f3a8
masterxyth <rizwanq@sfu.ca>
2017-04-17 11:32:38 -0700

    reverting to old

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import *
 import pandas as pd
-import csv
+
 app = Flask(__name__)
 
 @app.route("/") #Redirect link
 
 def show_tables():
-    data = pd.read_csv('standing-nominees.csv') #reading the CSV file #Removing column header name
-    riding = pd.read_csv('riding.csv')
-    riding = [tuple(x) for x in riding.to_records(index=False)]
-    return render_template('index.html',riding=riding, #returning view.html file from templates, using the data returned as a table in html with Class Data
+    data = pd.read_csv('standing-nominees.csv') #reading the CSV file
+    data.index.name=None #Removing column header name
+    return render_template('index.html',tables=[data.to_html(classes='data')], #returning view.html file from templates, using the data returned as a table in html with Class Data
     titles = ['Candidates']) #Table Title
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-candidate = pd.read_csv('candidate.csv')
-riding = pd.read_csv('riding.csv')
-riding = [tuple(x) for x in riding.to_records(index=False)]
-candidate = [tuple(x) for x in candidate.to_records(index=False)]
-
-riding
--- static/script.js
+++ static/script.js
@@ -36,93 +36,17 @@ function search() {
 }
 }
 
-
-
-
-
-
-var all_td = document.getElementsByTagName('td');
-var all_th = document.getElementsByTagName('th');
-var all_tr = document.getElementsByTagName('tr');
-
-for (var i = 0; i < all_tr.length; i++) {
-  var row = document.getElementsByTagName('tr')[0];
-  var div = document.createElement('div');
-  div.innerHTML = row.innerHTML;
-  row.parentNode.insertBefore(div,row);
-}
-
-for (i=0; i<all_tr.length;i++){
-  var tr = document.getElementsByTagName('tr')[0];
-  var parent = tr.parentNode;
-  parent.removeChild(tr);
-}
-
-/*
-for (i=0; i<all_th.length;i++) {
-  var th = document.getElementsByTagName('th')[0];
-  var parent = th.parentNode;
-  parent.removeChild(th);
-}
-
-*/
-/*
-for (var i = 1; i < all_td.length; i++) {
-  var row = document.getElementsByTagName('td')[i-1];
-  var div = document.createElement('div');
-  div.innerHTML = row.innerHTML;
-  row.parentNode.insertBefore(div,row);
-}
-
-for (i=0; i<all_td.length;i++){
-  var td = document.getElementsByTagName('td')[0];
-  var parent = td.parentNode;
-  parent.removeChild(td);
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
 // Create column classes
-
-
-/*for ( var i = 0; i <td.length-3; i+=3) {
+var td = document.getElementsByTagName('td');
+for ( var i = 0; i <td.length-3; i+=3) {
   td[i].className = 'riding_column';
   td[i+1].className = 'candidate_column';
   td[i+2].className = 'party_column';
 }
-*/
 
+// creating the 'row class'
 
-
-/*
-// creating the 'row classs
 var rows = document.getElementsByTagName('tr');
 for (var i = 0; i<rows.length; i++) {
   rows[i].className = 'row';
 }
-
-*/
-
-
-
-/*
-for (var i = 0; i<td.length; i++) {
-  var removeEls = document.getElementsByTagName('td')[i];
-  var containerEL = removeEls.parentNode;
-  containerEL.removeChild(removeEls);
-}
-
-/*var e = document.getElementsByTagName('td')[0];
-var d = document.createElement('div');
-d.innerHTML = e.innerHTML;
-e.parentNode.insertBefore(d, e);
-e.parentNode.removeChild(e);*/
